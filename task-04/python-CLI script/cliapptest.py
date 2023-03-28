1708e8062b96549dbbe017044feec1b5b643efe9
Nayan MK <nayanmkrishna@gmail.com>
2023-03-28 14:49:37 +0530

    initial changes

--- task-04/python-CLI script/cliapptest.py
+++ task-04/python-CLI script/cliapptest.py
@@ -3,34 +3,26 @@ import requests
 import urllib.request
 
 parser = argparse.ArgumentParser(description='fetch image from rover')
-parser.add_argument('id',type=int,help='Enter id')
-parser.add_argument('date',help='Enter date')
+parser.add_argument('id', type=int, help='Enter id')
+parser.add_argument('date', help='Enter date')
 args = parser.parse_args()
 id = args.id
-date = str(args.date)
-url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=lbFu4M0evI2JQX1tIh7ziCJILXMUUs2FrbffJNmf'
+date = args.date
+api_key = 'lbFu4M0evI2JQX1tIh7ziCJILXMUUs2FrbffJNmf'
+url = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={date}&api_key={api_key}'
 r = requests.get(url)
-data = r.json()
-data = data['photos']
-i = 0
-while True:
-    try:
+data = r.json()['photos']
 
-      if data[i]['id'] == id:
-          urllib.request.urlretrieve(data[i]['img_src'], '/Users/Nayan/newimage.jpg ')
-          break
-      elif data[i]['date'] == date:
-          urllib.request.urlretrieve(data[i]['img_src'], '/Users/Nayan/newimage.jpg ')
-          break
+found = False
+for photo in data:
+    if photo['id'] == id:
+        urllib.request.urlretrieve(photo['img_src'], 'newimage.jpg')
+        found = True
+        break
+    elif photo['earth_date'] == date:
+        urllib.request.urlretrieve(photo['img_src'], 'newimage.jpg')
+        found = True
+        break
 
-
-      i += 1
-    except EOFError:
-      break
-          
-
-
-
-
-
-
+if not found:
+    print('No image found with the given ID or date')
